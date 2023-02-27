f2482d7c414e9b0b7b829e7bfd8e5343a42424de
Arrizal Amin <arrizalamin@gmail.com>
2023-02-27 13:06:59 +0700

    fix action graph hook

--- packages/actions/src/action-graph.ts
+++ packages/actions/src/action-graph.ts
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLazyExpression } from "@batiq/expression";
+import { evaluateExpression, useLazyExpression } from "@batiq/expression";
 import Dot from "dot-object";
 
 const dot = new Dot(".");
@@ -10,39 +10,46 @@ type ActionGraph = {
   errorEdges: [number, number][];
 };
 
+export const createActionGraph = (
+  graph: ActionGraph,
+  evaluator: typeof evaluateExpression = evaluateExpression
+) => {
+  const run = (node = 0, ctx: Record<string, any> = {}) => {
+    const next = graph.successEdges.find((e) => e[0] === node)?.[1];
+    const error = graph.errorEdges.find((e) => e[0] === node)?.[1];
+
+    const evaluateWithCtx = (expression: string) =>
+      evaluator(expression, {
+        variables: Object.entries(dot.dot(ctx)).map(([key, value]) => ({
+          name: key,
+          type:
+            typeof value === "string"
+              ? "string"
+              : typeof value === "number"
+              ? "number"
+              : typeof value === "boolean"
+              ? "boolean"
+              : "any",
+          evaluate: () => value,
+        })),
+      });
+    let task = graph.nodes[node](evaluateWithCtx).then((run) => run());
+    if (next) {
+      task = task.then((result) => run(next, { ...ctx, result }));
+    }
+    if (error) {
+      task = task.catch(() => run(error, ctx));
+    }
+    return task;
+  };
+  return run;
+};
+
 export const useActionGraph = (graph: ActionGraph) => {
   const evaluate = useLazyExpression();
 
-  const run = React.useCallback(
-    async (node = 0, ctx = {}) => {
-      const next = graph.successEdges.find((e) => e[0] === node)?.[1];
-      const error = graph.errorEdges.find((e) => e[0] === node)?.[1];
-
-      const evaluateWithCtx = (expression: string) =>
-        evaluate(expression, {
-          variables: Object.entries(dot.dot(ctx)).map(([key, value]) => ({
-            name: key,
-            type:
-              typeof value === "string"
-                ? "string"
-                : typeof value === "number"
-                ? "number"
-                : typeof value === "boolean"
-                ? "boolean"
-                : "any",
-            evaluate: () => value,
-          })),
-        });
-      let task = graph.nodes[node](evaluateWithCtx).then((run) => run());
-      if (next) {
-        task = task.then((result) => run(next, { ...ctx, result }));
-      }
-      if (error) {
-        task = task.catch(() => run(error, ctx));
-      }
-      return task;
-    },
+  return React.useCallback(
+    () => createActionGraph(graph, evaluate)(0),
     [evaluate]
   );
-  return run;
 };
